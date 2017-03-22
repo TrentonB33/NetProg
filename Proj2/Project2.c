b63a270c6658bb871ea544750baa42ddad96cbf1
Aaron Brooks <AaronPBrooks@outlook.com>
2017-03-22 15:29:02 -0400

    starts child method

--- Proj2/Project2.c
+++ Proj2/Project2.c
@@ -1,7 +1,19 @@
 // I'm a file yay
 
-int main (int arc, char[] argv)
+int Child_Process(int pipe_fds, int sock_fd)
+{
+	TFTP *Packet
+	nbytes = read
+	return 1;
+}
+
+int main (int argc, char **argv)
 {
 	
 	return 1;
 }
+
+
+
+
+
