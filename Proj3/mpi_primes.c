c4bdd309c104b13433564f86ddd94e9607d2fbcd
DESKTOP-8VD9HUF\Aaron <AaronPBrooks@outlook.com>
2017-04-13 21:54:22 -0400

    Adds framework stuff

--- Proj3/mpi_primes.c
+++ Proj3/mpi_primes.c
@@ -4,6 +4,13 @@
 #include <stdint.h>
 #include <signal.h>
 
+struct Wheel {
+	int wheel_size;
+	int entries;
+	int** arr;	
+}
+
+
 int end_now = 0;
 
 void sig_handler(int signo)
@@ -33,3 +40,57 @@ int main(int argc, char **argv)
     
     return 0;
 }
+
+
+/**
+	
+
+**/
+
+int[] Wheel_Factorize(int _start, int _end, Wheel* wheel)
+{
+	int start = _start;
+	int end = _end;
+	int x = 0, arrsize = 10, entries = 0;
+	int 
+	if(start%2==0)
+	{
+		start--;
+	}
+	if(end%2==0)
+	{
+		end++;
+	}
+	while(x<=end)
+	{
+		for(x=0; x<wheel->entries;x++)
+		{
+			
+		}
+	}
+	return NULL;
+}
+
+void Add_To_Int_Array(int** array, int arraySize, int entries)
+{
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
